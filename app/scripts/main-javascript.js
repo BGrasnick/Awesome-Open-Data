0185ea3bb2874c894b3b72997c4259d31c3a260d
Bastien Grasnick <bastiengrasnick@arcor.de>
2014-04-30 16:57:59 +0200

    add simple d3 bar chart

--- app/scripts/main-javascript.js
+++ app/scripts/main-javascript.js
@@ -1,1 +1,12 @@
 console.log('\'Allo \'Allo from Javascript!');
+
+var data = [4, 8, 15, 16, 23, 42];
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .style("background-color", "#469d44")
+    .style("color", "white")
+    .text(function(d) { return d; });
