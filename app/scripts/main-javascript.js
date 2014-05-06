0bc3cfb3b0881ede4aa837d6012d9f7744c6f98e
Torsten Rüter <torsten@rueter.net>
2014-05-06 21:38:30 +0200

    Store map as backbone view
    This makes the map an object-oriented component.
    It will be created inside inside init function that is called when jquery document.ready fires. Once it is initialized it will render itself. It will re-render once the window has been resized, but not before the user finished doing so.
    This prevents flickering as some browsers fire the resize event multiple times per second.
    @see http://drupalmotion.com/article/debounce-and-throttle-visual-explanation

--- app/scripts/main-javascript.js
+++ app/scripts/main-javascript.js
@@ -1,58 +1,17 @@
-console.log('\'Allo \'Allo from Javascript!');
+window.opendata = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function(){
+        Backbone.history.start()
 
-var width = 960,
-    height = 580;
- 
-var color = d3.scale.category10();
- 
-var projection = d3.geo.mercator()
-    .scale(170)
-    .translate([width / 2, height / 2])
-    .precision(.1);
- 
-var path = d3.geo.path()
-    .projection(projection);
- 
-var graticule = d3.geo.graticule();
- 
-var svg = d3.select("#map").append("svg")
-    .attr("width", width)
-    .attr("height", height);
- 
-svg.append("defs").append("path")
-    .datum({type: "Sphere"})
-    .attr("id", "sphere")
-    .attr("d", path);
- 
-svg.append("use")
-    .attr("class", "stroke")
-    .attr("xlink:href", "#sphere");
- 
-svg.append("use")
-    .attr("class", "fill")
-    .attr("xlink:href", "#sphere");
- 
-svg.append("path")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("d", path);
+        opendata.Map = new opendata.Views.Map()
+    }
+}
 
-d3.json("data/world-110m.json", function(error, world) {
-  var countries = topojson.feature(world, world.objects.countries).features,
-      neighbors = topojson.neighbors(world.objects.countries.geometries);
+$( function(){
 
-  svg.selectAll(".country")
-      .data(countries)
-    .enter().insert("path", ".graticule")
-      .attr("class", "country")
-      .attr("d", path)
-      .style("fill", function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); });
- 
-  svg.insert("path", ".graticule")
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-      .attr("class", "boundary")
-      .attr("d", path);
+    opendata.init()
 
-});
- 
-d3.select(self.frameElement).style("height", height + "px");
+} );
---
+++ app/scripts/views/map.js
@@ -0,0 +1,85 @@
+opendata.Views = opendata.Views || {};
+
+(function () {
+    'use strict';
+
+    opendata.Views.Map = Backbone.View.extend({
+
+        el: '#map',
+
+        events: {},
+
+        initialize: function () {
+
+            // When the user resizes the window:
+            // only call render when he did not resize it anymore for 300 ms
+            var debouncedRender = _.debounce(this.render, 300);
+            $(window).on("resize.rerender", _.bind(debouncedRender, this));
+
+            // render the first time when the map is created
+            this.render()
+        },
+
+        render: function () {
+
+            this.$el.empty()
+
+            var width = this.$el.outerWidth(),
+                height = this.$el.outerHeight();
+
+            var color = d3.scale.category10();
+
+            var projection = d3.geo.mercator()
+                .scale(170)
+                .translate([width / 2, height / 2])
+                .precision(.1);
+
+            var path = d3.geo.path()
+                .projection(projection);
+
+            var graticule = d3.geo.graticule();
+
+            var svg = d3.select("#" + this.el.id).append("svg")
+                .attr("width", width)
+                .attr("height", height);
+
+            svg.append("defs").append("path")
+                .datum({type: "Sphere"})
+                .attr("id", "sphere")
+                .attr("d", path);
+
+
+            svg.append("use")
+                .attr("class", "fill")
+                .attr("xlink:href", "#sphere");
+
+            svg.append("path")
+                .datum(graticule)
+                .attr("class", "graticule")
+                .attr("d", path);
+
+            d3.json("data/world-110m.json", function(error, world) {
+                var countries = topojson.feature(world, world.objects.countries).features,
+                    neighbors = topojson.neighbors(world.objects.countries.geometries);
+
+                svg.selectAll(".country")
+                    .data(countries)
+                    .enter().insert("path", ".graticule")
+                    .attr("class", "country")
+                    .attr("d", path)
+                    .style("fill", function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); });
+
+                svg.insert("path", ".graticule")
+                    .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+                    .attr("class", "boundary")
+                    .attr("d", path);
+
+            });
+
+            d3.select(self.frameElement).style("height", height + "px");
+
+        }
+
+    });
+
+})();
