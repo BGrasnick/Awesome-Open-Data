dacf4b54353f95caccde766e0dd7b2f38c07376a
Bastien Grasnick <bastiengrasnick@arcor.de>
2014-05-06 16:43:52 +0200

    implemented https://gist.github.com/mbostock/4180634

--- app/scripts/main-javascript.js
+++ app/scripts/main-javascript.js
@@ -1,12 +1,67 @@
 console.log('\'Allo \'Allo from Javascript!');
 
-var data = [4, 8, 15, 16, 23, 42];
+//var data = [4, 8, 15, 16, 23, 42];
+// d3.select(".chart")
+//   .selectAll("div")
+//     .data(data)
+//   .enter().append("div")
+//     .style("width", function(d) { return d * 10 + "px"; })
+//     .style("background-color", "#469d44")
+//     .style("color", "white")
+//     .text(function(d) { return d; });
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return d * 10 + "px"; })
-    .style("background-color", "#469d44")
-    .style("color", "white")
-    .text(function(d) { return d; });
+var width = 960,
+    height = 580;
+ 
+var color = d3.scale.category10();
+ 
+var projection = d3.geo.mercator()
+    .scale(170)
+    .translate([width / 2, height / 2])
+    .precision(.1);
+ 
+var path = d3.geo.path()
+    .projection(projection);
+ 
+var graticule = d3.geo.graticule();
+ 
+var svg = d3.select(".chart").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+ 
+svg.append("defs").append("path")
+    .datum({type: "Sphere"})
+    .attr("id", "sphere")
+    .attr("d", path);
+ 
+svg.append("use")
+    .attr("class", "stroke")
+    .attr("xlink:href", "#sphere");
+ 
+svg.append("use")
+    .attr("class", "fill")
+    .attr("xlink:href", "#sphere");
+ 
+svg.append("path")
+    .datum(graticule)
+    .attr("class", "graticule")
+    .attr("d", path);
+
+d3.json("world-110m.json", function(error, world) {
+  var countries = topojson.feature(world, world.objects.countries).features,
+      neighbors = topojson.neighbors(world.objects.countries.geometries);
+ 
+  svg.selectAll(".country")
+      .data(countries)
+    .enter().insert("path", ".graticule")
+      .attr("class", "country")
+      .attr("d", path)
+      .style("fill", function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); });
+ 
+  svg.insert("path", ".graticule")
+      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+      .attr("class", "boundary")
+      .attr("d", path);
+});
+ 
+d3.select(self.frameElement).style("height", height + "px");
