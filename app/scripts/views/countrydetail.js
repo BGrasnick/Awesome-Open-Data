d64e16f3820076d1ac3665028074b0f6edababd5
Torsten Rüter <torsten@rueter.net>
2014-05-28 14:54:40 +0200

    Show cannabis bars on detail countries

--- app/scripts/views/countrydetail.js
+++ app/scripts/views/countrydetail.js
@@ -13,6 +13,7 @@ opendata.Views = opendata.Views || {};
 
         initialize: function () {
             this.currentCountry = null;
+            this.currentDrug = 'cannabis';
 
             _.bindAll(this, 'setCountry');
         },
@@ -25,7 +26,26 @@ opendata.Views = opendata.Views || {};
             }
 
             this.$el.html(this.template( this.currentCountry.toJSON() ));
-            console.log(this.currentCountry)
+
+            this.renderChart();
+
+        },
+
+        renderChart : function() {
+            var that = this;
+
+            var data = this.currentCountry.get('drugs')[that.currentDrug];
+
+            var x = d3.scale.linear()
+                .domain( [0, d3.max(data, function(d){ return d.prevalence })] )
+                .range( [0, 290] );
+
+            d3.select( that.$('.chart')[0] ).selectAll('div')
+              .data(data)
+            .enter().append("div")
+              .style("width", function(d) { return x(d.prevalence) + 'px'; })
+              .attr("class", "bar-" + that.currentDrug)
+              .text(function(d) { return d.population + ": " + d.prevalence + "%"; });
 
         },
 
