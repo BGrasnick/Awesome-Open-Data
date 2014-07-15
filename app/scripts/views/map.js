426c745b303aa959df08fac5cc862fc988203ace
Torsten Rüter <torsten@rueter.net>
2014-07-15 17:32:36 +0200

    Fix Europe

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -31,44 +31,6 @@ opendata.Views = opendata.Views || {};
 
         render: function () {          
 
-            var europeIdArray = [
-              620,
-              724,
-              250,
-              826,
-              372,
-              578,
-              246,
-              752,
-              276,
-              380,
-              208,
-              528,
-              56,
-              616,
-              203,
-              40,
-              703,
-              792,
-              348,
-              705,
-              191,
-              300,
-              100,
-              642,
-              196,
-              440,
-              428,
-              // fill empty holes, unfortunately no data
-              756,
-              442,
-              70,
-              688,
-              807,
-              8,
-              499
-            ];
-
             this.$el.empty();
 
             var that = this;
@@ -118,8 +80,7 @@ opendata.Views = opendata.Views || {};
 
             d3.json("data/world.topo.json", function(error, world) {
 
-                // var neighbours = topojson.neighbors( world.objects.countries.geometries)
-
+                // Countries
                 that.g.selectAll(".country")
                   .data( topojson.feature( world, world.objects.countries ).features )
                 .enter().insert("path", ".graticule")
@@ -135,16 +96,20 @@ opendata.Views = opendata.Views || {};
                   .attr("class", "boundary")
                   .attr("d", path);
 
+                // Europe
                 that.g.append("path")
-                  .datum(topojson.merge(world, world.objects.countries.geometries.filter(function (d) { return _.contains(europeIdArray,d.id) ? d : null;})))
+                  .datum( that.mergeEurope(world) )
+                  .attr("class", that.getClasses)
                   .attr("id", "europeOverlay")
-                  .style("fill", "#5BCC8D")
+                  .style("fill", that.requestCountryColor)
+                  .attr("country-id", function( d ) { return d.id })
                   .attr("d", path)
                   .on("click", clicked);
 
             });
 
             d3.json("/data/us.topo.json", function(error, us) {
+                // US
                 that.g.append("g")
                   .attr("id", "states")
                   .selectAll(".state")
@@ -178,7 +143,7 @@ opendata.Views = opendata.Views || {};
                 var bounds = path.bounds(d),
                     dx = bounds[1][0] - bounds[0][0],
                     dy = bounds[1][1] - bounds[0][1];
-
+                
                 // Sets a maximum zoom level
                 if(dx < 25) dx = 40;
 
@@ -265,6 +230,25 @@ opendata.Views = opendata.Views || {};
             } else
                 return currentColorScale( country.get(currentFilter) );
 
+        },
+
+        mergeEurope: function( world ){
+
+            var europeCountryIds = [
+                620, 724, 250, 826, 372, 578,
+                246, 752, 276, 703, 792, 348,
+                380, 208, 528,  56, 616, 203,
+                705, 191, 300, 100, 642, 196,
+                 40, 440, 428,
+                // fill empty holes, unfortunately no data
+                756, 442, 70, 688, 807, 8, 499
+            ];
+
+            var europe = topojson.merge( world, world.objects.countries.geometries.filter( function (d) {
+                return _.contains( europeCountryIds, d.id )
+            }));
+
+            return _.extend(europe, { id: 1 })
         }
 
     });
